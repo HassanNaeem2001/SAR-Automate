ba28bb263356ce9f369c01f3b51991bb7bdcfb02
HassanNaeem2001 <hassannaeem4590@gmail.com>
2024-08-28 14:39:43 +0500

    28 August 2024

--- resources/views/admindash/addbatch.blade.php
+++ resources/views/admindash/addbatch.blade.php
@@ -5,6 +5,27 @@
         margin-top:20px
     }
 </style>
+<div
+    class="alert alert-primary alert-dismissible fade show"
+    role="alert"
+>
+    <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+    ></button>
+    <strong>Batch Added</strong>Successfully !
+</div>
+
+<script>
+    var alertList = document.querySelectorAll(".alert");
+    alertList.forEach(function (alert) {
+        new bootstrap.Alert(alert);
+    });
+</script>
+
+
 <h1>Add Batch</h1>
 <hr>
 <form action="" method="post">
@@ -12,6 +33,24 @@
     <div class="inputs">
     <label for="batchcode">Enter Batch Code</label>
     <input type="text" name="batchode" class="form-control">
+    </div>
+    <div class="inputs">
+    <label for="timings">Select Timings</label>
+    <select name="timings" id="" class="form-control">
+        <option value="">9-11</option>
+        <option value="">11-1</option>
+        <option value="">1-3</option>
+        <option value="">3-5</option>
+        <option value="">5-7</option>
+        <option value="">7-9</option>
+    </select>
+    </div>
+    <div class="inputs">
+        <label for="date">Start Date</label>
+        <input type="date" class="form-control">
+    </div>
+    <div class="inputs">
+        <button class="btn btn-primary w-100">Add Batch</button>
     </div>
 </form>
 @endsection
