3497876c82917ebd5a3cda260585fa7a321205ca
Hassan Naeem <hassannaeem4590@gmail.com>
2025-02-18 19:10:46 +0500

    Views Created On 18th Feb 2025

---
+++ resources/views/StudentViews/dashboard.blade.php
@@ -0,0 +1,24 @@
+@extends('StudentBasicNav.dashboard')
+@section('content')
+<div class="container-fluid mt-5">
+<h3 class="text-center">Student Dashboard</h3>
+<hr>
+<div class="container-fluid">
+<div class="row">
+    <div class="col-lg-4 mt-2   ">
+        <img src="Images/personimage.jpg" class="w-100" alt="" height="260px">
+    </div>
+    <div class="col-lg-8 mt-2">
+        <p><b>Student ID:</b> 123456</p>
+        <p><b>Course:</b> Computer Science</p>
+        <p><b>Curriculum:</b> Computer Science</p>
+        <p><b>Year:</b> 2</p> 
+        <p><b>Faculty:</b> Science</p>
+        <p><b>Current Skill:</b> </p>
+        <p><b>Status:</b> </p>
+       
+    </div>
+</div>
+</div>
+</div>
+@endsection
